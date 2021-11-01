4058f71aa9b7f7c6698b2f6611d633e3d6d0f692
Nguyen Khac Dung <etcrv@gmail.com>
2021-11-01 10:15:11 +0700

    Push code

---
+++ main.js
@@ -0,0 +1,35 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+
+
+
+
+
+const app = {
+
+    slider: [
+        {
+            name: 'slider 1',
+            image: './assets/img/bodyweb/shopee_mall/1.jfif'
+        },
+        {
+            name: 'slider 2',
+            image: './assets/img/bodyweb/shopee_mall/slider2.png'
+        },
+        {
+            name: 'slider 3',
+            image: './assets/img/bodyweb/shopee_mall/slider3.png'
+        },
+        {
+            name: 'slider 4',
+            image: './assets/img/bodyweb/shopee_mall/slider4.png'
+        }
+    ],
+
+    start: function(){
+        
+    }
+
+}
+
+app.start();
